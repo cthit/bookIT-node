1257c870d45d0e5b516e3e4d4eb5d474c716fa33
x183 <palmoscar4@gmail.com>
2022-05-04 12:29:38 +0200

    yes

--- backend/src/resolvers/party_report.resolver.ts
+++ backend/src/resolvers/party_report.resolver.ts
@@ -1,23 +1,24 @@
 import { Tools } from "../utils/commonTypes";
 import { Event } from "../models/event";
 import { getPartyReport } from "../services/party_report.service";
+import { User } from "../models";
 
 export const getPartyReportQResolvers = ({ prisma }: Tools) => ({
   party_reports: () => {
     return prisma.party_report.findMany();
   },
-  party_report: async (_: any, { id }: { id: string }) => {
-    return getPartyReport(prisma, id);
+  party_report: async (_: any, { id }: { id: string },{user}:{user:User}) => {
+    return getPartyReport(prisma, id,user);
   },
 });
 
 export const getPartyReportResolvers = ({ prisma }: Tools) => ({
   Event: {
-    party_report: async ({ party_report_id }: Event, args: any) => {
+    party_report: async ({ party_report_id }: Event,{user}:{user:User}, args: any) => {
       if (!party_report_id) {
         return null;
       }
-      return getPartyReport(prisma, party_report_id);
+      return getPartyReport(prisma, party_report_id,user);
     },
   },
 });
