81d70641d3c7936e3953cc9590a7fe3d8d33e5b7
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2022-05-17 16:37:51 +0200

    Updated to better solution

--- backend/src/resolvers/illegal_slots.resolver.ts
+++ backend/src/resolvers/illegal_slots.resolver.ts
@@ -10,8 +10,8 @@ export const getIllegalSlotsQResolvers = ({ prisma }: Tools) => ({
     const from = new Date(ft.from);
     const to = new Date(ft.to);
 
-    const rules = await (await getRulesBetween(prisma, from, to)).filter(rule => !rule.allow);
+    const rules = await (await getRulesBetween(prisma, from, to))
 
-    return mergeRules(toMiniRules(rules, from, to));
+    return mergeRules(toMiniRules(rules, from, to)).filter(rule => !rule.allow);
   },
 });
