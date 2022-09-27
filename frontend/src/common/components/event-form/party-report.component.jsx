c7da83b7a299f2c7a0ed0167b4d5c250ce172de4
x183 <palmoscar4@gmail.com>
2022-09-27 20:58:27 +0200

    Added medansvarig

--- frontend/src/common/components/event-form/party-report.component.jsx
+++ frontend/src/common/components/event-form/party-report.component.jsx
@@ -13,6 +13,9 @@ const PartyReport = ({ init }) => {
   const repNameValues = ff("responsible_name");
   const repNumberValues = ff("responsible_number");
   const repEmailValues = ff("responsible_email");
+  const co_repNameValues = ff("co_responsible_name");
+  const co_repNumberValues = ff("co_responsible_number");
+  const co_repEmailValues = ff("co_responsible_email");
   const [texts] = useDigitTranslations(translations);
 
   return (
@@ -26,20 +29,36 @@ const PartyReport = ({ init }) => {
         <>
           <DigitCheckbox {...permitValues} label={texts.permit} />
 
-          <DigitLayout.Column>
-            <DigitTextField
-              {...repNameValues}
-              upperLabel={texts.responsible_name}
-            />
-            <DigitTextField
-              {...repNumberValues}
-              upperLabel={texts.responsible_number}
-            />
-            <DigitTextField
-              {...repEmailValues}
-              upperLabel={texts.responsible_email}
-            />
-          </DigitLayout.Column>
+          <DigitLayout.Row>
+            <DigitLayout.Column>
+              <DigitTextField
+                {...repNameValues}
+                upperLabel={texts.responsible_name}
+              />
+              <DigitTextField
+                {...repNumberValues}
+                upperLabel={texts.responsible_number}
+              />
+              <DigitTextField
+                {...repEmailValues}
+                upperLabel={texts.responsible_email}
+              />
+            </DigitLayout.Column>
+            <DigitLayout.Column>
+              <DigitTextField
+                {...co_repNameValues}
+                upperLabel={texts.co_responsible_name}
+              />
+              <DigitTextField
+                {...co_repNumberValues}
+                upperLabel={texts.co_responsible_number}
+              />
+              <DigitTextField
+                {...co_repEmailValues}
+                upperLabel={texts.co_responsible_email}
+              />
+            </DigitLayout.Column>
+          </DigitLayout.Row>
         </>
       )}
     </>
