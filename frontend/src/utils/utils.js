c2c6e72fadd0b9b0a726e0198f3368881804da95
x183 <palmoscar4@gmail.com>
2022-09-30 22:55:01 +0200

    Fixed formatting of date and time

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,5 @@
 import moment from "moment";
 
 export const formatDT = date => moment(Date(date)).format("yyyy-MM-DD HH:mm");
-export const formatDate = date => moment(Date(date)).format("yyyy-MM-DD");
-export const formatTime = date => moment(Date(date)).format("HH:mm");
+export const formatDate = date => moment(date).format("yyyy-MM-DD");
+export const formatTime = date => moment(date).format("HH:mm");
