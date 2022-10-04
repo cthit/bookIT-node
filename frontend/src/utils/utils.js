2afecf873aaba564cec67af6f248dc8d0bd142cf
x183 <palmoscar4@gmail.com>
2022-10-04 15:34:48 +0200

    Fixed start before end date

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,10 @@
 import moment from "moment";
 
-export const formatDT = date => moment(Date(date)).format("yyyy-MM-DD HH:mm");
+export const formatDT = date => {
+	console.log(date);
+	if(Number.isNaN(Number.parseInt(date)))
+		return moment(date).format("yyyy-MM-DD HH:mm");
+	return moment(Date(Number.parseInt(date))).format("yyyy-MM-DD HH:mm");
+}
 export const formatDate = date => moment(date).format("yyyy-MM-DD");
 export const formatTime = date => moment(date).format("HH:mm");
