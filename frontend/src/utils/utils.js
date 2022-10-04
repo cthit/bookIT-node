62e436b003a9db0d166329697a0c4b73dc72818f
x183 <palmoscar4@gmail.com>
2022-10-04 17:13:16 +0200

    Fixed all dates being the same

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -3,7 +3,7 @@ import moment from "moment";
 export const formatDT = date => {
 	if(Number.isNaN(Number.parseInt(date)))
 		return moment(date).format("yyyy-MM-DD HH:mm");
-	return moment(Date(Number.parseInt(date))).format("yyyy-MM-DD HH:mm");
+	return moment(new Date(Number.parseInt(date))).format("yyyy-MM-DD HH:mm");
 }
 export const formatDate = date => moment(date).format("yyyy-MM-DD");
 export const formatTime = date => moment(date).format("HH:mm");
